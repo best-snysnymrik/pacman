28f227a2781ec1bfa072f63127622fca14796aa3
best-snysnymrik <bestsnysnymrik@gmail.com>
2015-10-16 08:13:21 +0700

    добавлено моргание привидений, когда они из напуганного состояния возвращаются в обычное

--- pacman/Assets/Scripts/Pacman/View/Units/Behavior/FrighteningBehavior.cs
+++ pacman/Assets/Scripts/Pacman/View/Units/Behavior/FrighteningBehavior.cs
@@ -1,15 +1,32 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
 namespace Pacman.View.Units.Behavior
 {
+	public class ColorPeriod
+	{
+		public float time;
+		public Color color;
+		
+		public ColorPeriod(float time, Color color)
+		{
+			this.time = time;
+			this.color  = color;
+		}
+	}
+	
 	public class FrighteningBehavior : MonoBehaviour
 	{
 		private Color frighteningColor = new Color(0.016f, 0.207f, 0.773f, 1f);
 		private Color normalColor;
 		
+		private const float flashTime = 0.15f;
+		private const int flashCount = 5;
+		
+		private Queue<ColorPeriod> colorPeriods = new Queue<ColorPeriod>();
+		private ColorPeriod currentColorPeriod;
+		
 		private bool isFrighteningMode = false;
-		private float frighteningBehaviorTimer = 0;
 		
 		void Awake()
 		{}
@@ -21,17 +38,39 @@ namespace Pacman.View.Units.Behavior
 		
 		public void StartFrighteningBehavior(float time)
 		{
-			frighteningBehaviorTimer = time;
-			renderer.material.SetColor("_Color", frighteningColor);
+			CreateColorQueue(time);
+			ApplyNextColorPeriod();
 			
 			isFrighteningMode = true;
 		}
 		
+		private void CreateColorQueue(float time)
+		{
+			colorPeriods.Enqueue(new ColorPeriod(time - 2 * flashCount * flashTime, frighteningColor));
+			
+			for (int i = 0; i < flashCount; ++i)
+			{
+				colorPeriods.Enqueue(new ColorPeriod(flashTime, normalColor));
+				colorPeriods.Enqueue(new ColorPeriod(flashTime, frighteningColor));
+			}
+		}
+		
+		private void ApplyNextColorPeriod()
+		{
+			if (colorPeriods.Count == 0)
+				currentColorPeriod = null;
+			else
+			{
+				currentColorPeriod = colorPeriods.Dequeue();
+				renderer.material.SetColor("_Color", currentColorPeriod.color);
+			}
+		}
+		
 		public void StopFrighteningBehavior()
 		{
-			renderer.material.SetColor("_Color", normalColor);
-			
 			isFrighteningMode = false;
+			colorPeriods.Clear();
+			renderer.material.SetColor("_Color", normalColor);			
 		}
 		
 		void FixedUpdate()
@@ -39,13 +78,17 @@ namespace Pacman.View.Units.Behavior
 			if (!isFrighteningMode)
 				return;
 			
-			frighteningBehaviorTimer -= Time.deltaTime;
-			
-			if (frighteningBehaviorTimer <= 0)
+			if (currentColorPeriod == null)
 			{
 				isFrighteningMode = false;
 				renderer.material.SetColor("_Color", normalColor);
+				return;
 			}
+
+			currentColorPeriod.time -= Time.deltaTime;
+			
+			if (currentColorPeriod.time <= 0)
+				ApplyNextColorPeriod();
 		}
 	}
 }
